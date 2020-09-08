b8496ae0343b9f44a6353f48a5f423caa277e400
C7LF <ccal72@yahoo.co.uk>
2020-09-08 18:58:39 +0100

    create cookie and redis session

--- ba-reddit-server/src/index.ts
+++ ba-reddit-server/src/index.ts
@@ -10,6 +10,14 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
 const main = async () => {
   // connect to db
   const orm = await MikroORM.init(microConfig);
@@ -19,12 +27,32 @@ const main = async () => {
   const app = express();
   const port = 4000;
 
+  // session middlware runs before apollo middleware so it can be used inside apollo
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 34 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "asdfqwer",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
--- ba-reddit-server/src/types.ts
+++ ba-reddit-server/src/types.ts
@@ -1,5 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
 
 export type MyContext = {
-    em: EntityManager<IDatabaseDriver<Connection>>
-}
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Express.Session }; // join two types, set session to required rather than default optional
+  res: Response;
+};
