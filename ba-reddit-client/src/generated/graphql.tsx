9fa721437c4e1eb9c54bb3f17e33be6916691bf1
C7LF <ccal72@yahoo.co.uk>
2020-09-30 21:08:52 +0100

    Post list display UI

--- ba-reddit-client/src/generated/graphql.tsx
+++ ba-reddit-client/src/generated/graphql.tsx
@@ -238,7 +238,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title'>
+    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'content'>
   )> }
 );
 
@@ -351,6 +351,7 @@ export const PostsDocument = gql`
     createdAt
     updatedAt
     title
+    content
   }
 }
     `;
--- ba-reddit-client/src/pages/index.tsx
+++ ba-reddit-client/src/pages/index.tsx
@@ -2,31 +2,49 @@ import React from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
-import { Heading, Link, Spinner } from "@chakra-ui/core";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Spinner,
+  Stack,
+  Text,
+} from "@chakra-ui/core";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery({
     variables: {
-      limit: 10
-    }
+      limit: 10,
+    },
   });
   return (
     <Layout varient="regular">
-      <Heading>Hello!</Heading>
-      <NextLink href="/create-post">
-        <Link>Create Post</Link>
-      </NextLink>
+      <Flex>
+        <Heading>Hello!</Heading>
+        <NextLink href="/create-post">
+          <Link>
+            <Button variantColor="teal" ml={5} size="md">
+              Create Post
+            </Button>
+          </Link>
+        </NextLink>
+      </Flex>
       <br />
       {!data ? (
         <Spinner />
       ) : (
-        data.posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-          </div>
-        ))
+        <Stack spacing={8}>
+          {data.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.content}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
