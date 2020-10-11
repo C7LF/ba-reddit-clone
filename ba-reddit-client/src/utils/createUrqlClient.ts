4464ab4db4ddd504ea5cb650a9ff1d73518ed2c5
C7LF <ccal72@yahoo.co.uk>
2020-10-11 17:21:52 +0100

    points read and write fragment to clear cache

--- ba-reddit-client/src/utils/createUrqlClient.ts
+++ ba-reddit-client/src/utils/createUrqlClient.ts
@@ -12,10 +12,12 @@ import {
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 import Router from "next/router";
 import { stringify } from "querystring";
+import gql from "graphql-tag";
 
 // Error exchange for error handeling
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
@@ -148,6 +150,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              { id: postId } as any
+            ); // Data or null
+            if (data) {
+              const newPoints = (data.points as number) + value;
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                { id: postId, points: newPoints } as any
+              );
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields("Query");
             const fieldInfos = allFields.filter(
