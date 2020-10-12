98a08ff94b00597beb2b61f4581dc3c7467177f2
C7LF <ccal72@yahoo.co.uk>
2020-10-12 21:04:40 +0100

    Delete post - FK issue

--- ba-reddit-client/src/pages/post/[id].tsx
+++ ba-reddit-client/src/pages/post/[id].tsx
@@ -1,9 +1,9 @@
-import { Breadcrumb, Divider, Heading } from "@chakra-ui/core";
+import { Divider, Heading, IconButton, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import React from "react";
 import { Layout } from "../../components/Layout";
-import { usePostQuery } from "../../generated/graphql";
+import { useDeletePostMutation, usePostQuery } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 
 export const Post = ({}) => {
@@ -17,20 +17,34 @@ export const Post = ({}) => {
       id: intId,
     },
   });
+  const [, deletePost] = useDeletePostMutation();
   if (fetching) {
     return <Layout varient="regular">Loading...</Layout>;
   }
 
-  if(!data?.post) {
-    return <Layout varient="regular">Could not find post</Layout>
+  if (!data?.post) {
+    return <Layout varient="regular">Could not find post</Layout>;
   }
 
   return (
     <Layout varient="regular">
+      <Text color="gray.500" fontWeight="light" letterSpacing="wide">
+        {data.post.creator.username}
+      </Text>
       <Heading mb={5}>{data.post.title}</Heading>
       <p>{data.post.content}</p>
+      <Divider />
+      <IconButton
+        icon="delete"
+        aria-label="delete post"
+        color="darkred"
+        onClick={() => {
+          // @ts-ignore
+          deletePost({ id: data.post?.id });
+        }}
+      ></IconButton>
     </Layout>
-  )
+  );
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
